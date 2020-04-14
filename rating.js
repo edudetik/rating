d1062885f6b1ba1e0f69dae70f12f208731d50a9
edudetik <50113460+edudetik@users.noreply.github.com>
2020-04-14 23:37:36 +0700

    Add files via upload

---
+++ rating.js
@@ -0,0 +1,25 @@
+<script type="application/ld+json">
+{
+"@context": "https://schema.org/",
+"@type": "Review",
+"itemReviewed": {
+"@type": "Thing",
+
+ 
+"name": "<data:blog.pageName/>"
+},
+"author": {
+"@type": "Person",
+"name": "indodetik"
+},
+"reviewRating": {
+"@type": "Rating",
+"ratingValue": "4.8",
+"bestRating": "5"
+},
+"publisher": {
+"@type": "Organization",
+"name": "indodetik"
+}
+}
+</script>
